57d1733130c968248cdc309f8db012589ef4f584
t1.kashehi <kashehi2003@gmail.com>
2024-02-05 15:19:10 +0330

    Edit Models And View

--- src/SampleProject.ViewModels/Product/AddProductViewModel.cs
+++ src/SampleProject.ViewModels/Product/AddProductViewModel.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SampleProject.ViewModels.Product
+﻿namespace SampleProject.ViewModels.Product
 {
     public class AddProductViewModel
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
         public double? Price { get; set; }
+
         public int? Count { get; set; }
-        public string Model { get; set; }
-        public string Color { get; set; }
-        public string Mission { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Color { get; set; }
+
+        public string? Mission { get; set; }
 
     }
 }
--- src/SampleProject.ViewModels/Product/ShowProductViewModel.cs
+++ src/SampleProject.ViewModels/Product/ShowProductViewModel.cs
@@ -1,14 +1,22 @@
-﻿namespace SampleProject.ViewModels.Product
+﻿
+namespace SampleProject.ViewModels.Product
 {
     public class ShowProductViewModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
         public double? Price { get; set; }
+
         public int? Count { get; set; }
-        public string Model { get; set; }
-        public string Color { get; set; }
-        public string Mission { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Color { get; set; }
+
+        public string? Mission { get; set; }
     }
 }
