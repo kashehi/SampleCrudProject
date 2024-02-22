39b0599857e9ba4c105fb40897701b55139f07a5
t1.kashehi <kashehi2003@gmail.com>
2024-02-22 12:11:08 +0330

    add validation for Product

--- src/SampleProject.ViewModels/Product/AddProductViewModel.cs
+++ src/SampleProject.ViewModels/Product/AddProductViewModel.cs
@@ -1,21 +1,37 @@
-﻿namespace SampleProject.ViewModels.Product
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleProject.ViewModels.Product
 {
     public class AddProductViewModel
     {
-
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "عنوان محصول")]
         public string? Title { get; set; }
 
+        [Display(Name = "توضیحات")]
         public string? Description { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "قیمت")]
         public double? Price { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "تعداد")]
         public int? Count { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "مدل")]
         public string? Model { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "رنگ")]
         public string? Color { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "ابعاد")]
         public string? Mission { get; set; }
+
+
 
     }
 }
--- src/SampleProject.ViewModels/Product/UpdateProductViewModel.cs
+++ src/SampleProject.ViewModels/Product/UpdateProductViewModel.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace SampleProject.ViewModels.Product
 {
     public class UpdateProductViewModel
@@ -6,18 +8,31 @@ namespace SampleProject.ViewModels.Product
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "عنوان محصول")]
         public string? Title { get; set; }
 
+        [Display(Name = "توضیحات")]
         public string? Description { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "قیمت")]
         public double? Price { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "تعداد")]
         public int? Count { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "مدل")]
         public string? Model { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "رنگ")]
         public string? Color { get; set; }
 
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [Display(Name = "ابعاد")]
         public string? Mission { get; set; }
     }
 }
